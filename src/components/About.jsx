89940d7ebeebbdb42e394951af84543b24d58ddf
PrabhuP1 <prabhupugal01@gmail.com>
2023-03-06 20:42:11 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,14 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-20'>
-                lorem ipsum dolor sit amet, consectetur adipis
+              I am currently in my 3rd year of my M.Sc Software Systems Course.
+              I am studying at <strong><em>Coimbatore Institute of Technology, Coimbatore.</em></strong></p>
+              <p className='text-xl mt-5'>
+              I did my schooling at <b><i>Nirmala Matha Convent Matriculation Higher Secondary School, Coimbatore.</i></b>
+              My short term goal is to get internship as a Software Developer in any firm. And my long term goal is to work in a dream company that pays 30LPA.
+              In these two years time to get prepared and become a Industry ready developer.
             </p>
             <br/>
-            <p className='text-xl'>Thelorem 50 </p>
         </div>
     </div>
   )
